f16c0c1067008b101e6870c86d8d4c021c3f587a
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-02-10 17:02:55 +0000

    exercises

--- directory.rb
+++ directory.rb
@@ -23,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  i = 0
+  while i < students.length
+    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    i += 1
   end
 end
 
