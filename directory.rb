5cd4f4a80bc9a90ec9d6d2556490d0e966bf5a81
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-02-11 16:09:21 +0000

    8.7 cohort requires user input

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students".center(50)
-  puts "To finish, just hit return twice".center(50)
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
@@ -8,14 +8,19 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    puts "Please enter your height".center(50)
+    puts "Please enter your height"
     height = gets.chomp
-    puts "Please enter your hobby".center(50)
+    puts "Please enter your hobby"
     hobby = gets.chomp
-    students << {name: name, height: height, hobby: hobby, cohort: :november}
-    puts "Now we have #{students.count} students".center(50)
+    puts "Please enter your cohort"
+    cohort = gets.chomp
+    if cohort == ""
+      cohort = "default"
+    end
+    students << {name: name, height: height, hobby: hobby, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} students"
     # get another name from the user
-    puts "Enter the next student".center(50)
+    puts "Enter the next student"
     name = gets.chomp
   end
   #return the array of students
@@ -23,20 +28,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "-------------".center(50)
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(students)
   i = 0
   while i < students.length
-    puts "#{i + 1}. #{students[i][:name]}, height: #{students[i][:height]}, hobby: #{students[i][:hobby]}, (#{students[i][:cohort]} cohort)".center(50)
+    puts "#{i + 1}. #{students[i][:name]}, height: #{students[i][:height]}, hobby: #{students[i][:hobby]}, (#{students[i][:cohort]} cohort)"
     i += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(50)
+  puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
