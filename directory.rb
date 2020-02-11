e86c1df7b0c70c0b3f7c2e876b1f31fe8ad0041a
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-02-11 15:45:31 +0000

    centered puts statements (8.6)

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(50)
+  puts "To finish, just hit return twice".center(50)
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp  
   #while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Please enter your height".center(50)
+    height = gets.chomp
+    puts "Please enter your hobby".center(50)
+    hobby = gets.chomp
+    students << {name: name, height: height, hobby: hobby, cohort: :november}
+    puts "Now we have #{students.count} students".center(50)
     # get another name from the user
+    puts "Enter the next student".center(50)
     name = gets.chomp
   end
   #return the array of students
@@ -18,20 +23,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(students)
   i = 0
   while i < students.length
-    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{i + 1}. #{students[i][:name]}, height: #{students[i][:height]}, hobby: #{students[i][:hobby]}, (#{students[i][:cohort]} cohort)".center(50)
     i += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50)
 end
 
 students = input_students
